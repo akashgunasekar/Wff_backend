9bbfe05f7a90001605d5b26b6de4edabf36f1866
Foragefoxoffice <actewpdeveloper1@gmail.com>
2026-09-08 17:51:58 +0530

    fix: resolve Hostinger username prefix u841181565_wff

--- config/config.php
+++ config/config.php
@@ -24,7 +24,11 @@ if (file_exists($envFile)) {
 define('DB_HOST', $envVars['DB_HOST'] ?? $_ENV['DB_HOST'] ?? getenv('DB_HOST') ?: 'localhost');
 define('DB_PORT', $envVars['DB_PORT'] ?? $_ENV['DB_PORT'] ?? getenv('DB_PORT') ?: '3306');
 define('DB_NAME', $envVars['DB_NAME'] ?? $_ENV['DB_NAME'] ?? getenv('DB_NAME') ?: 'u841181565_wff_tn');
-define('DB_USER', $envVars['DB_USER'] ?? $_ENV['DB_USER'] ?? getenv('DB_USER') ?: 'wff');
+$dbUser = $envVars['DB_USER'] ?? $_ENV['DB_USER'] ?? getenv('DB_USER') ?: 'u841181565_wff';
+if ($dbUser === 'wff') {
+    $dbUser = 'u841181565_wff';
+}
+define('DB_USER', $dbUser);
 define('DB_PASS', $envVars['DB_PASS'] ?? $envVars['DB_PASSWORD'] ?? $_ENV['DB_PASS'] ?? $_ENV['DB_PASSWORD'] ?? (getenv('DB_PASS') ?: 'Wff@#2k26'));
 
 define('RAZORPAY_KEY_ID', $envVars['RAZORPAY_KEY_ID'] ?? $_ENV['RAZORPAY_KEY_ID'] ?? getenv('RAZORPAY_KEY_ID') ?: '');
